a23245e53c37cca639e8612b2199316dce74e94b
tomhowarth76 <tomhowarth@mac.com>
2019-10-16 16:38:43 +0100

    just an update but this time with a method

--- directory.rb
+++ directory.rb
@@ -1,18 +1,18 @@
 student_count = 11
 
-puts "The students of Villains Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-#anything after a hash is ignored
-
-puts "Overall, we have #{student_count} great students"
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Tom Howarth",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates",
+]
+students.each { |students| puts students }
+puts "Overall, we have #{students.count} great students"
